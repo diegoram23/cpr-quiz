a64b716df3e4d9153078bd9f8f270f362d9a7767
diegoram23 <diego.pta.oc@gmail.com>
2023-04-24 15:34:50 -0700

    answers change to green if true and red if false

--- index.js
+++ index.js
@@ -8,26 +8,54 @@ const nextBtn = document.getElementById('next-btn')
 let score = 0
 let currentQuestionIndex = 0
 
+document.addEventListener('click', (e) => {
+    if (e.target) {
+        handleAnswerClick(e.target)
+    }
+})
+
+const handleAnswerClick = (id) => {
+
+    const isCorrect = id.dataset.correct === 'true'
+    const isFalse = id.dataset.correct === 'false'
+
+    if (isCorrect) {
+        id.style.backgroundColor = '#00a36c'
+        console.log('yes')
+    } if (isFalse) {
+        id.style.backgroundColor = '#ff3131'
+        console.log('no')
+    }
+}
+
 const startQuiz = () => {
     currentQuestionIndex = 0
     score = 0
     nextBtn.innerHTML = 'Next'
-    showQuestion()
+    getShowQuestionHtml()
 }
 
+const getShowQuestionHtml = () => {
+    let questionHtml = ''
 
-const showQuestion = () => {
     let currentQuestion = questions[currentQuestionIndex]
     let questionNumber = currentQuestionIndex + 1
     questionEl.innerHTML = questionNumber + '. ' + currentQuestion.question
 
+    //loops through questions.answers data and creates html to later be called
     currentQuestion.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerHTML = answer.text
-        button.classList.add('btn')
-        answerBtn.appendChild(button)
-    });
+        questionHtml += `
+        <button class="btn" data-correct='${answer.correct}'>${answer.text}</button>
+        `
+    })
+
+
+    return questionHtml
 }
 
+//call and renders possible answers to question displayed
+document.getElementById('answer-buttons').innerHTML = getShowQuestionHtml()
+
+//initializes the app
 startQuiz()
 

6b75c387386c692e40a1097322032ad8cb3bd59c
diegoram23 <diego.pta.oc@gmail.com>
2023-04-25 11:54:01 -0700

    Able to play again at end of questions

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const answerBtn = document.getElementById('answer-buttons')
 const nextBtn = document.getElementById('next-btn')
 const result = document.getElementById('result')
 const scoreboard = document.getElementById('scoreboard')
+const mainContent = document.getElementById('quiz')
+const againBtn = document.getElementById('again-btn')
 
 let score = 0
 let currentQuestionIndex = 0
@@ -19,15 +21,21 @@ document.addEventListener('click', (e) => {
     if (e.target.dataset.next) {
         handleNextBtn()
     }
+
+    if (e.target.dataset.again) {
+        
+    }
 })
 
-
+againBtn.addEventListener('click', () => {
+    location.reload()
+})
 
 // Handles button clicks
 const handleAnswerClick = (id) => {
     const isCorrect = id.dataset.correct === 'true'
 
-    // Changes background color if answer choosen
+    // If user chooses correct answer
     if (isCorrect) {
         id.style.backgroundColor = '#00a36c'
         // Increments score
@@ -38,6 +46,7 @@ const handleAnswerClick = (id) => {
         result.textContent = 'Correct!'
         result.style.color = '#00a36c'
 
+        // If user chooses incorrect answer
     } else {
         id.style.backgroundColor = '#ff2400'
         handleScore()
@@ -62,33 +71,46 @@ const handleAnswerClick = (id) => {
 
 const handleScore = () => {
     //Increments score by 1
-   scoreboard.innerHTML = `Score: ${score} out of ${questions.length}`
+    scoreboard.innerHTML = `Score: ${score} out of ${currentQuestionIndex + 1}`
 }
 
 // Advances to next question
 const handleNextBtn = () => {
     // Hides result message
     result.style.display = 'none'
+    nextBtn.style.display = 'none'
     currentQuestionIndex++
-    getShowQuestionHtml()
+
+    // Calls for next quesion only if questions are remaining otherwise calls endgame
+    currentQuestionIndex < questions.length ? getShowQuestionHtml() : endGame()
+}
+
+const endGame = () => {
+    againBtn.style.display = 'block'
+    mainContent.innerHTML = `
+            <h2>Your final score is ${score} out of ${currentQuestionIndex}</h2>
+            <button id="again-btn" data-again='again-btn'>Again</button>
+    `
 }
 
 // Collects html for current question and possible answers
 const getShowQuestionHtml = () => {
-    let questionHtml = ''
-
     let currentQuestion = questions[currentQuestionIndex]
+
     let questionNumber = currentQuestionIndex + 1
     questionEl.innerHTML = questionNumber + '. ' + currentQuestion.question
 
+    let questionHtml = ''
     //loops through questions.answers data and creates html to later be called
     currentQuestion.answers.forEach(answer => {
         questionHtml += `
         <button class="btn" data-correct='${answer.correct}'>${answer.text}</button>
         `
-
-        document.getElementById('answer-buttons').innerHTML = questionHtml
+        return questionHtml
     })
+    
+    document.getElementById('answer-buttons').innerHTML = questionHtml
+
 }
 
 // Starts quiz from the beginning with 0 score

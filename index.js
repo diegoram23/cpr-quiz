7975780366b2a9f3c3c04cb12dd6087170c2c76d
diegoram23 <diego.pta.oc@gmail.com>
2023-04-25 10:57:35 -0700

    Incrementing and displaying score

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { questions } from "/questions.js";
 const questionEl = document.getElementById('question')
 const answerBtn = document.getElementById('answer-buttons')
 const nextBtn = document.getElementById('next-btn')
+const result = document.getElementById('result')
+const scoreboard = document.getElementById('scoreboard')
 
 let score = 0
 let currentQuestionIndex = 0
@@ -28,9 +30,21 @@ const handleAnswerClick = (id) => {
     // Changes background color if answer choosen
     if (isCorrect) {
         id.style.backgroundColor = '#00a36c'
+        // Increments score
         score++
+        handleScore()
+        // Message displaying incorrect
+        result.style.display = 'block'
+        result.textContent = 'Correct!'
+        result.style.color = '#00a36c'
+
     } else {
         id.style.backgroundColor = '#ff2400'
+        handleScore()
+        // Message displaying incorrect
+        result.style.display = 'block'
+        result.textContent = 'Incorrect'
+        result.style.color = '#ff2400'
     }
 
     // Next button appears after first answer click
@@ -46,18 +60,20 @@ const handleAnswerClick = (id) => {
     })
 }
 
+const handleScore = () => {
+    //Increments score by 1
+   scoreboard.innerHTML = `Score: ${score} out of ${questions.length}`
+}
+
+// Advances to next question
 const handleNextBtn = () => {
+    // Hides result message
+    result.style.display = 'none'
     currentQuestionIndex++
     getShowQuestionHtml()
-    console.log(currentQuestionIndex, 'here')
 }
 
-const startQuiz = () => {
-    score = 0
-    currentQuestionIndex = 0
-    getShowQuestionHtml()
-}
-
+// Collects html for current question and possible answers
 const getShowQuestionHtml = () => {
     let questionHtml = ''
 
@@ -70,10 +86,18 @@ const getShowQuestionHtml = () => {
         questionHtml += `
         <button class="btn" data-correct='${answer.correct}'>${answer.text}</button>
         `
-        
+
         document.getElementById('answer-buttons').innerHTML = questionHtml
     })
 }
+
+// Starts quiz from the beginning with 0 score
+const startQuiz = () => {
+    score = 0
+    currentQuestionIndex = 0
+    getShowQuestionHtml()
+}
+
 //initializes the app
 startQuiz()
 
